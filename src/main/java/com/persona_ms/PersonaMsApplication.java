2bea8fa5b1c6a017a34858079d8f7488568b4456
pierre.rojas_pragma <pier.rojas@pragma.com.co>
2025-06-09 10:57:34 -0500

    Proyecto base

---
+++ src/main/java/com/persona_ms/PersonaMsApplication.java
@@ -0,0 +1,13 @@
+package com.persona_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonaMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonaMsApplication.class, args);
+    }
+
+}
